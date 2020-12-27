ff2c8024995444b439f76c53727a3719d08fdf0a
ArmanR9 <armanr09@gmail.com>
2020-12-26 23:00:01 -0800

    Added procfile for Heroku deployment

--- main.js
+++ main.js
@@ -7,6 +7,25 @@
 /* @return -> Tuple with two solutions (Access via res.x1 and res.x2)
 */
 
+const plt = require('matplotnode');
+const x = new Array(100).fill(0).map((x, i) => i / Math.PI);
+
+// xkcd-style plot :)
+plt.xkcd();
+
+plt.subplot("211");
+plt.title('trig');
+plt.plot(x, x.map(Math.sin), 'color=r', 'label=sin(x)');
+plt.plot(x, x.map(Math.cos), 'color=g', 'label=cos(xß)');
+plt.legend();
+
+plt.subplot("212");
+plt.plot(x, x.map(Math.sin).map((t, i) => t * i), 'color=b', 'label=x * sin(x)', 'marker=o', 'linestyle=None');
+plt.legend();
+plt.ylim(-100, 100);
+
+plt.save("./subplot.png");
+
 function quadratic_calculator(a, b, c){
 
     if(a === undefined || b === undefined || c == undefined)
